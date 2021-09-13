da48a847602b2b79df1c005f9d6eefd4fc4dff92
JDgomez2002 <gom21429@uvg.edu.gt>
2021-09-13 13:17:42 -0600

    Fase 1
    Creación de Clases y gitignore.

---
+++ DriverProgram.java
@@ -0,0 +1,20 @@
+//Universidad del Valle de Guatemala
+//Programación Orientada a Objetos
+//Catedrático Tomás Gálvez
+//Segundo Semestre 2021
+//José Daniel Gómez Cabrera
+//Carné 21429
+//Sección 11
+//Actividad: Laboratorio 2
+
+/**
+ * Controlador del programa.
+ * 
+ * @author José Daniel Gómez Cabrera
+ * @version DriverProgram 1.1
+ */
+public class DriverProgram{
+    public static void main(String[] args){
+        
+    }
+}
---
+++ SDR.java
@@ -0,0 +1,18 @@
+//Universidad del Valle de Guatemala
+//Programación Orientada a Objetos
+//Catedrático Tomás Gálvez
+//Segundo Semestre 2021
+//José Daniel Gómez Cabrera
+//Carné 21429
+//Sección 11
+//Actividad: Laboratorio 2
+
+/**
+ * Clase SDR.
+ * 
+ * @author José Daniel Gómez Cabrera
+ * @version SDR Class 1.1
+ */
+public class SDR {
+    
+}
