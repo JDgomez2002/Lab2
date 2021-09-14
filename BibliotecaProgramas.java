aa520a76820b416d6a5a7b8f9907628293491a07
JDgomez2002 <gom21429@uvg.edu.gt>
2021-09-13 22:01:51 -0600

    Fase 2
    Inicio de programa con clases Programa y BibliotecaProgramas. y el archivo de texto con la información de los programas.

--- BibliotecaProgramas.java
+++ BibliotecaProgramas.java
@@ -7,6 +7,9 @@
 //Sección 11
 //Actividad: Laboratorio 2
 
+import java.util.Scanner;
+import java.io.File;
+
 /**
  * Clase BibliotecaProgramas.
  * 
@@ -14,5 +17,119 @@
  * @version BibliotecaProgramas Class 1.1
  */
 public class BibliotecaProgramas {
-    
+    Programa[] programas_disponibles;
+
+    /**
+     * Constructor de BibliotecaProgramas.
+     * 
+     * @author José Daniel Gómez Cabrera
+     * @version BibliotecaProgramas 1.1
+     */
+    public BibliotecaProgramas(){
+        try{
+            this.programas_disponibles = file_reader();
+        }
+        catch(Exception e){
+            String s = "BibliotecaProgramas: Constructor: "+e.getMessage();
+            throw new RuntimeException(s);
+        }
+    }
+
+    /**
+     * Lector de archivo programas.txt.
+     * 
+     * @author José Daniel Gómez Cabrera
+     * @version file_reader 1.1
+     * @return Programa[]
+     */
+    private static Programa[] file_reader(){
+        try{
+            File programas_txt = new File("programas.txt");
+            Scanner scan1 = new Scanner(programas_txt);
+            String programas = "";
+            while(scan1.hasNextLine()){
+                programas = programas.concat(scan1.nextLine()+":");
+            }
+            String[] programas_separados = programas.split(":");
+            Programa[] array_programas = new Programa[programas_separados.length];
+            for(int k = 0; k<array_programas.length; k++){
+                String[] info_programa = programas_separados[k].split(",");
+                Programa mi_programa = new Programa(info_programa[0], Integer.parseInt(info_programa[1]), Integer.parseInt(info_programa[2]));
+                array_programas[k] = mi_programa;
+            }
+            return array_programas;
+        }
+        catch(Exception e){
+            String s = "file_reader: "+e.getMessage();
+            throw new RuntimeException(s);
+        }
+    }
+
+    /**
+     * Metodo para ingresar un numero, y regresar un objeto programa de la lista.
+     * 
+     * @author José Daniel Gómez Cabrera
+     * @version obtener_Programa 1.1
+     * @return Programa
+     * @param int (indice de programa deseado)
+     */
+    public Programa obtener_Programa(int no_programa){  
+        Programa mi_programa = this.programas_disponibles[6];
+        String programa_buscado = "";
+        switch (no_programa) {
+            case 1:
+                programa_buscado = "GOOGLE CHROME";
+                break;
+            
+            case 2:
+            programa_buscado = "FIREFOX";
+                break;
+
+            case 3:
+            programa_buscado = "ZOOM";
+                break;
+            
+            case 4:
+            programa_buscado = "WHATSAPP";
+                break;
+
+            case 5:
+            programa_buscado = "TELEGRAM";
+                break;
+            
+            case 6:
+            programa_buscado = "VISUAL STUDIO";
+                break;
+
+            case 7:
+            programa_buscado = "MAIL";
+                break;
+
+            case 8:
+            programa_buscado = "CONTACTOS";
+                break;
+      
+            default:
+            programa_buscado = "WORD";
+                break;
+        }
+
+        for(int i =0; i<this.programas_disponibles.length; i++){
+            if(programas_disponibles[i].get_nombre_programa().equals(programa_buscado)){
+                mi_programa = programas_disponibles[i];
+            }
+        }
+        return mi_programa;
+    }
+
+    // /**
+    //  * getter de Programa[].
+    //  * 
+    //  * @author José Daniel Gómez Cabrera
+    //  * @version get_programas 1.1
+    //  * @return Programa[]
+    //  */
+    // public Programa[] get_programas(){
+    //     return this.programas_disponibles;
+    // }
 }
--- Programa.java
+++ Programa.java
@@ -14,5 +14,113 @@
  * @version Programa Class 1.1
  */
 public class Programa {
+    private String nombre;
+    private int espacio_memoria;
+    private int ciclos_reloj;
+    private int ciclos_restantes;
+    private int bloques_memoria;
+
+    /**
+     * Constructor de Programa.
+     * 
+     * @author José Daniel Gómez Cabrera
+     * @version Programa 1.1
+     */
+    public Programa(String nombre, int espacio_memoria, int ciclos_reloj){
+        try{
+            this.nombre = nombre;
+            this.espacio_memoria = espacio_memoria;
+            this.ciclos_reloj = ciclos_reloj;
+            this.ciclos_restantes = ciclos_reloj;
+            this.bloques_memoria = (obtener_bloques_memoria(espacio_memoria));
+        }
+        catch(Exception e){
+            String s = "Programa: Constructor Programa: "+e.getMessage();
+            throw new RuntimeException(s);
+        }
+    }
+
+    /**
+     * Actualiza los ciclos de reloj restantes para saber cuando retirar el programa de la memoria RAM.
+     * 
+     * @author José Daniel Gómez Cabrera
+     * @version actualizar_ciclos_restantes 1.1
+     */
+    public void actualizar_ciclos_restantes(){
+        try{
+            this.ciclos_restantes -= 1;
+        }
+        catch(Exception e){
+            String s = "Programa: actualizar_ciclos_restantes: "+e.getMessage();
+            throw new RuntimeException(s);
+        }
+    }
+
+    /**
+     * Obtiene la cantidad de bloques de memoria del programa a partir de la cantidad de megabytes.
+     * Si la division tiene residuo, significa que ocupa debe de ocupar un bloque mas de la division.
+     * por ejemplo, 2070 megabytes (cada bloque son 64 megabytes) (2070/64=32.3), tendria que ocupar 33 bloques.
+     * 
+     * @author José Daniel Gómez Cabrera
+     * @version obtener_bloques_memoria 1.1
+     * @return int
+     * @param megabytes (int)
+     */
+    private int obtener_bloques_memoria(int megabytes){
+        int residue = (megabytes%64);
+        int bloques_memoria;
+        if(residue==0){
+            bloques_memoria = (megabytes/64);
+        }
+        else{
+            bloques_memoria = (megabytes/64)+1;
+        }
+        return bloques_memoria;
+    }
+
+    /**
+     * getter de nombre.
+     * 
+     * @author José Daniel Gómez Cabrera
+     * @version get_nombre_programa 1.1
+     * @return String
+     */
+    public String get_nombre_programa(){
+        return this.nombre;
+    }
+
+    /**
+     * getter de megabytes de espacio en memoria.
+     * 
+     * @author José Daniel Gómez Cabrera
+     * @version get_megabytes 1.1
+     * @return int
+     */
+    public int get_megabytes(){
+        return this.espacio_memoria;
+    }
+
+    /**
+     * getter de ciclos de reloj restantes.
+     * 
+     * @author José Daniel Gómez Cabrera
+     * @version get_ciclos_restantes 1.1
+     * @return int
+     */
+    public int get_ciclos_restantes(){
+        return this.ciclos_restantes;
+    }
+
+    /**
+     * getter de cantidad de bloques de memoria que ocupa el programa.
+     * 
+     * @author José Daniel Gómez Cabrera
+     * @version get_bloques 1.1
+     * @return int
+     */
+    public int get_bloques(){
+        return this.bloques_memoria;
+    }
+
     
 }
